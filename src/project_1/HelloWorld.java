e1b7f983c32e03e65f0a9873fcff16e2f19ba35b
ubshik <ubshik@gmail.com>
2016-10-11 19:22:14 +0300

    initial commit

---
+++ src/project_1/HelloWorld.java
@@ -0,0 +1,10 @@
+package project_1;
+
+/**
+ * Created by StudentTC on 06.10.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
